875ff1d5fb5772a0cd1ba8e42807507575ebde30
Sirth <47194064+thenzler@users.noreply.github.com>
2024-12-17 15:11:50 +0100

    Add layout component

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
